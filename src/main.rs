c48c5af4f7ccadf4c234f5b5ac2abc5ebef0117c
Javon Harper <javon.d.harper@gmail.com>
2024-11-06 23:46:53 -0500

    Basic implementation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
-// use colored::*;
+use rand::prelude::*;
 use reqwest::blocking::Client;
-// use reqwest::Error;
 use serde::{Deserialize, Serialize};
-// use std::env;
 use std::io;
+
+const TOKEN: &str = "";
+
+#[derive(Debug, Serialize, Deserialize)]
+struct TelevisionEpisodeResponse {
+    episode_number: i32,
+    name: String,
+    overview: String,
+    runtime: i32,
+    season_number: i32,
+    id: i32,
+}
 
 struct TelevisionEpisode {
     show_name: String,
@@ -37,6 +47,7 @@ struct TVSeriesDetailsSeason {
 struct ShowInfo {
     id: i32,
     name: String,
+    origin_country: Vec<String>,
 }
 
 #[derive(Debug)]
@@ -48,7 +59,7 @@ struct ShowOption {
 
 fn main() {
     println!("Welcome to rand-watch");
-    println!("Type in a show name to get a random episode");
+    println!("Enter a show name to get a random episode");
 
     println!("");
     let mut query = String::new();
@@ -67,34 +78,60 @@ fn main() {
 
     println!("");
     println!("Enter the number of the show you want to watch");
+    println!("");
     let mut show_number = String::new();
-    let _show = io::stdin()
+    io::stdin()
         .read_line(&mut show_number)
         .expect("Failed to read line");
     println!("");
 
-    println!("Finding an episode...");
+    // let show_number = show_number.trim();
+    println!(
+        "Getting episode data for '{}'",
+        options[show_number.trim().parse::<usize>().unwrap() - 1].label
+    );
     println!("");
 
-    let episodes = fetch_more_data(&client, &options.first().unwrap().value);
+    let season_data = fetch_season_data(
+        &client,
+        &options[show_number.trim().parse::<usize>().unwrap() - 1].value,
+    );
+
+    let mut episodes = vec![];
+    for season in season_data.seasons.iter() {
+        for i in 1..=season.episode_count {
+            // Omit season 0
+            if season.season_number == 0 {
+                continue;
+            }
+            episodes.push((season.season_number, i));
+        }
+    }
+
+    let random_episode = episodes.choose(&mut rand::thread_rng()).unwrap();
+    let episode_response = fetch_episode(
+        &client,
+        &options[show_number.trim().parse::<usize>().unwrap() - 1].value,
+        random_episode.0,
+        random_episode.1,
+    );
 
     let episode = TelevisionEpisode {
-        show_name: String::from("The Office (US)"),
-        season: 6,
-        title: String::from("Scott's Tots"),
-        number: 12,
-        description: String::from("Two new, young employees throw Dwight and Jim off balance; Andy returns from leadership training."),
+        show_name: options.first().unwrap().label.clone(),
+        season: episode_response.season_number,
+        title: episode_response.name,
+        number: episode_response.episode_number,
+        description: episode_response.overview,
     };
 
     print_episode(episode);
 
     println!("");
     println!("Enjoy the show!");
+    println!("");
 }
 
 fn fetch_show_info(client: &Client, query: &str) -> Vec<ShowOption> {
-    let token = "";
-
     let url = format!(
         "https://api.themoviedb.org/3/search/tv?query={}&include_adult=false&language=en-US&page=1",
         query
@@ -102,7 +139,7 @@ fn fetch_show_info(client: &Client, query: &str) -> Vec<ShowOption> {
 
     let response: ShowInfoResponse = client
         .get(&url)
-        .bearer_auth(token)
+        .bearer_auth(TOKEN)
         .header("accept", "application/json")
         .send()
         .unwrap()
@@ -113,7 +150,10 @@ fn fetch_show_info(client: &Client, query: &str) -> Vec<ShowOption> {
     for (i, show) in response.results.iter().enumerate().take(5) {
         options.push(ShowOption {
             number: i + 1,
-            label: show.name.clone(),
+            // TODO: Can I only show country if multiple share the same name?
+            // XXX: Also happens with shows from same country but different year
+            // EG: Married with Children
+            label: format!("{} ({})", show.name.clone(), show.origin_country.join(", ")),
             value: show.id,
         })
     }
@@ -121,21 +161,43 @@ fn fetch_show_info(client: &Client, query: &str) -> Vec<ShowOption> {
     return options;
 }
 
-fn fetch_more_data(client: &Client, id: &i32) {
-    let token = "";
-
+fn fetch_season_data(client: &Client, id: &i32) -> TVSeriesDetailsResponse {
     let url = format!("https://api.themoviedb.org/3/tv/{}?language=en-US", id);
 
     let response: TVSeriesDetailsResponse = client
         .get(&url)
-        .bearer_auth(token)
+        .bearer_auth(TOKEN)
         .header("accept", "application/json")
         .send()
         .unwrap()
         .json()
         .unwrap();
 
-    println!("{:?}", response);
+    // println!("{:?}", response);
+    return response;
+}
+
+fn fetch_episode(
+    client: &Client,
+    id: &i32,
+    season: i32,
+    episode: i32,
+) -> TelevisionEpisodeResponse {
+    let url = format!(
+        "https://api.themoviedb.org/3/tv/{}/season/{}/episode/{}?language=en-US",
+        id, season, episode
+    );
+
+    let response: TelevisionEpisodeResponse = client
+        .get(&url)
+        .bearer_auth(TOKEN)
+        .header("accept", "application/json")
+        .send()
+        .unwrap()
+        .json()
+        .unwrap();
+
+    return response;
 }
 
 fn print_episode(episode: TelevisionEpisode) {
